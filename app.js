0d83007a30f43c62b2acdcd74c3d8c7a4439990a
Michael9621 <oandami@gmail.com>
2020-12-15 22:56:25 +0300

    db schema

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ const express = require('express')
 
 const app = express() //initialize app with express
 
+const mongoose = require("mongoose")
+
+//create posts schema
+const PostsSchema = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    desc: {
+        type: String
+    }
+})
+
+let Posts = mongoose.model('posts', PostsSchema)
+
 //create a vase route with a response
 app.get('/', (req,res) => {
     res.send('server running')
+})
+
+//route to get posts
+app.get('/getPosts', async (req, res) => {
+    const posts = await Posts.find()
+    res.json(posts)
 })
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-//require app.js
+const mongoose = require('mongoose')
 
+//require app.js
 const app = require('./app')
+
+require('dotenv').config({ path: '.env'})
+
+//database connection
+mongoose.connect(process.env.DATABASE,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }
+  );
+
+mongoose.Promise = global.Promise; // tell mongoose to use es6 promises
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Database connection error -> ${err.message} `)
+})
 
 //starting server on port 3000
 const server = app.listen(3000, () => {
